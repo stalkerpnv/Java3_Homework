4aecdcdab9b3a8b98075897420d11395bbc03e1d
stalkerpnv <stalker_pnv@mail.ru>
2019-04-20 21:11:21 +0900

    lesson1

---
+++ SimpleBox.java
@@ -0,0 +1,17 @@
+package lecture1;
+
+public class SimpleBox {
+    private Object obj;
+
+    public Object getObj() {
+        return obj;
+    }
+
+    public void setObj(Object obj) {
+        this.obj = obj;
+    }
+
+    public SimpleBox(Object obj) {
+        this.obj = obj;
+    }
+}
